7f759374035fb6969b029ff0d2b890484594eeab
Matin <matin.mn@hotmail.com>
2023-09-12 01:18:21 +0800

    ep7 | alpine js search box

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -5,7 +5,7 @@
         </div>
         <div id="side-bar"
             class="border-t border-t-gray-100 md:border-t-none col-span-4 md:col-span-1 px-3 md:px-6  space-y-10 py-6 pt-10 md:border-l border-gray-100 h-screen sticky top-0">
-            <livewire:search-box />
+            @include('posts.partials.search-box')
 
             <div id="recommended-topics-box">
                 <h3 class="text-lg font-semibold text-gray-900 mb-3">Recommended Topics</h3>
